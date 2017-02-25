f7379221777ea59e82f5d0bc4c17376807db1818
John Pillar <johneppillar@gmail.com>
2017-02-25 20:22:44 +0000

    Added more light skill endpoints

--- skills/lights/lights.js
+++ skills/lights/lights.js
@@ -18,7 +18,8 @@ function lightOnOff (req, res, next) {
           lightNumber   = req.query.light_number;
     
     var paramsOK    = false,
-        lightStatus = false;
+        lightStatus = false,
+        lightState  = hue.lightState;
 
     if (typeof lightNumber !== 'undefined' && lightNumber !== null) {
         paramsOK = true;
@@ -42,7 +43,8 @@ function lightOnOff (req, res, next) {
     };
 
     if(paramsOK) {
-        // Turn on or off the lights
+        // Turn on or off the light
+        //Hue.lightState.create().on().brightness(100)
         Hue.setLightState(lightNumber, {"on": lightStatus})
         .then(function(obj) {
             if (obj=true) {
@@ -60,18 +62,137 @@ function lightOnOff (req, res, next) {
 
             // Send response back to caller
             alfredHelper.sendResponse(res, 'error', err);
+            console.log('lightOnOff: ' + err);
         })
         .done();
     } else {
         // Send response back to caller
         alfredHelper.sendResponse(res, 'error', 'The parameters light_status or light_number was either not supplied or invalid.');
+        console.log('lightOnOff: The parameters light_status or light_number was either not supplied or invalid.');
     };
+    next();
+};
+
+//=========================================================
+// Skill: dimlight
+// Params: light_number: Number
+//=========================================================
+function dimLight (req, res, next) {
+
+    const HueBridgeIP   = process.env.HueBridgeIP,
+          HueBridgeUser = process.env.HueBridgeUser,
+          Hue           = new HueApi(HueBridgeIP, HueBridgeUser),
+          lightNumber   = req.query.light_number;
+
+    if (typeof lightNumber !== 'undefined' && lightNumber !== null) {
+        // Dim the light
+        Hue.setLightState(lightNumber, {'brightness': '30'})
+        .then(function(obj) {
+            if (obj=true) {
+                var returnMessage = 'The light was dimmed.',
+                    status = 'sucess';
+            } else {
+                var returnMessage = 'There was an error dimming the light.',
+                    status = 'error';
+                    console.log('dimLight: ' + returnMessage);
+            }
+
+            // Send response back to caller
+            alfredHelper.sendResponse(res, status, returnMessage);
+        })
+        .fail(function(err) {
+
+            // Send response back to caller
+            alfredHelper.sendResponse(res, 'error', err);
+            console.log('dimLight: ' + err);
+        })
+        .done();
+    } else {
+        // Send response back to caller
+        alfredHelper.sendResponse(res, 'error', 'The parameter light_number was not supplied.');
+        console.log('dimLight: The parameter light_number was not supplied.');
+    };
+    next();
+};
+
+//=========================================================
+// Skill: brightenLight
+// Params: light_number: Number
+//=========================================================
+function brightenLight (req, res, next) {
+
+    const HueBridgeIP   = process.env.HueBridgeIP,
+          HueBridgeUser = process.env.HueBridgeUser,
+          Hue           = new HueApi(HueBridgeIP, HueBridgeUser),
+          lightNumber   = req.query.light_number;
+
+    if (typeof lightNumber !== 'undefined' && lightNumber !== null) {
+        // Brighten the light
+        Hue.setLightState(lightNumber, {'brightness': '100'})
+        .then(function(obj) {
+            if (obj=true) {
+                var returnMessage = 'The light was brightened.',
+                    status = 'sucess';
+            } else {
+                var returnMessage = 'There was an error increasing the brightness.',
+                    status = 'error';
+                    console.log('dimLight: ' + returnMessage);
+            }
+
+            // Send response back to caller
+            alfredHelper.sendResponse(res, status, returnMessage);
+        })
+        .fail(function(err) {
+
+            // Send response back to caller
+            alfredHelper.sendResponse(res, 'error', err);
+            console.log('dimLight: ' + err);
+        })
+        .done();
+    } else {
+        // Send response back to caller
+        alfredHelper.sendResponse(res, 'error', 'The parameter light_number was not supplied.');
+        console.log('dimLight: The parameter light_number was not supplied.');
+    };
+    next();
+};
+
+//=========================================================
+// Skill: listLights
+//=========================================================
+function listLights (req, res, next) {
+
+    const HueBridgeIP   = process.env.HueBridgeIP,
+          HueBridgeUser = process.env.HueBridgeUser,
+          Hue           = new HueApi(HueBridgeIP, HueBridgeUser);
+
+    Hue.lights()
+    .then(function(obj) {
+
+        // Send response back to caller
+        alfredHelper.sendResponse(res, 'sucess', obj);
+
+    })
+    .fail(function(err) {
+
+        // Send response back to caller
+        alfredHelper.sendResponse(res, 'error', err);
+        console.log('listLights: ' + err);
+    })
+    .done();
+
     next();
 };
 
 //=========================================================
 // Add skills to server
 //=========================================================
-skill.get('/lightonoff', lightOnOff)
+skill.get('/lightonoff', lightOnOff);
+skill.get('/dimlight', dimLight);
+skill.get('/brightenlight', brightenLight);
+skill.get('/listlights', listLights);
+
+//TODO
+//Move mode
 
 module.exports = skill;
