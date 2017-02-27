4d5399ce51bd6708abba3c6be95005a93def8cf3
John Pillar <johneppillar@gmail.com>
2017-02-27 21:07:10 +0000

    Made time location based

--- skills/time/time.js
+++ skills/time/time.js
@@ -3,21 +3,50 @@
 //=========================================================
 const Skills       = require('restify-router').Router;  
       skill        = new Skills(),
-      alfredHelper = require('../../helper.js');
+      alfredHelper = require('../../helper.js'),
+      NodeGeocoder = require('node-geocoder'),
+      options      = {
+        provider: 'google',
+        httpAdapter: 'https', 
+        formatter: null         
+      },
+      geocoder     = NodeGeocoder(options),
+      dateFormat   = require('dateformat');
 
 //=========================================================
 // Skill: whatisthetime
 //=========================================================
 function whatisthetime (req, res, next) {
-    const time = new Date().toLocaleTimeString('en-GB', {
-        hour: 'numeric',
-        minute: 'numeric'
-    });
-    var responseText = 'The time is ' + time + '.';
 
-    // Send response back to caller
-    alfredHelper.sendResponse(res, 'sucess', responseText);
-    
+    // Get the location
+    var location = '';
+    if (typeof req.query.location !== 'undefined' && req.query.location !== null) {
+        location = req.query.location;
+    } else {
+        location = 'london';
+    };
+
+    geocoder.geocode(location)
+    .then(function(Data) {
+        var lat = Data[0].latitude,
+            lng = Data[0].longitude,
+            url = 'http://api.geonames.org/timezoneJSON?lat=' + lat + '&lng=' + lng + '&username=johneppillar';
+            alfredHelper.requestAPIdata(url)
+            .then(function(apiData){
+                returnMessage = 'The time in ' + location + ' is currently ' + dateFormat(apiData.body.time, "h:MM TT") + '.';
+                alfredHelper.sendResponse(res, 'sucess', returnMessage);
+            })
+            .catch(function (err) {
+                // Send response back to caller
+                alfredHelper.sendResponse(res, 'error', err.message);
+                console.log('whatisthetime: ' + err);
+            });
+    })
+    .catch(function(err) {
+        // Send response back to caller
+        alfredHelper.sendResponse(res, 'error', err);
+        console.log('whatisthetime: ' + err);
+    });
     next();
 };
 
