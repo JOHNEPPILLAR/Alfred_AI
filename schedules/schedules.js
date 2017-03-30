cb6abe7701dfd715c13ea1c1fd845e9715506e3f
John Pillar <johneppillar@gmail.com>
2017-03-30 20:07:26 +0100

    Finished refactor of schudules

--- schedules/schedules.js
+++ schedules/schedules.js
@@ -11,66 +11,10 @@ var appSchedules = function(server) {
           Hue           = new HueApi(HueBridgeIP, HueBridgeUser),
           lightState    = HueLights.lightState;
 
-    var sunRise   = new Date(),
-        sunSet    = new Date(),
-        firstRun  = true,
-        dailyTimer,
-        sunRiseTimer,
-        sunSetTimer,
-        
-        setSchedules = function() {
-        
-            //=========================================================
-            // Set the daily timers
-            //=========================================================
-            var currentTime      = dateFormat(new Date(), 'HH:MM'),
-                scheduleSettings = require('../scheduleSettings.json'),
-                setTime,
-                offTimers        = [],
-                turnOffTimes     = scheduleSettings.lightsOut;
-
-            console.log (currentTime + ' - Running daily scheduler');
-
-            // Get sunrise & sunset data
-            const url = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=' + process.env.OPENWEATHERMAPAPIKEY;
-            alfredHelper.requestAPIdata(url)
-            .then(function(apiData){
-
-                sunRise = new Date(apiData.body.sys.sunrise);
-                sunSet  = new Date(apiData.body.sys.sunset);
-                sunSet.setHours(sunSet.getHours() + 12); // Add 12 hrs as for some resion the api returnes it as am!
-
-                // Cancel existing timers
-                if (!firstRun){
-                    sunRiseTimer.cancel();
-                    sunSetTimer.cancel();
-                    firstRun = false;
-                    // TODO cancel off timers
-                };
-
-                // TODO reset sunRise if < 6 am
-
-                // Set new sunrise timer
-                sunRiseTimer = schedule.scheduleJob({hour: sunRise.getHours(), minute: sunRise.getMinutes()}, function(){
-                    turnOnLights();
-                });
-
-                // Set new sunset timer
-                sunSetTimer = schedule.scheduleJob({hour: sunSet.getHours(), minute: sunSet.getMinutes()}, function(){
-                    turnOnLights();
-                });
-
-                // set timers to turn off lights
-                turnOffTimes.forEach(function(value){
-                    sunSetTimer = schedule.scheduleJob({hour: value.hour, minute: value.minute}, function(){
-                        turnOffLights();
-                    });
-                });
-            })
-            .catch(function (err) {
-                console.log('Schedule get data Error: ' + err);
-            });
-        },
+    var rule         = new schedule.RecurrenceRule(),
+        sunRise      = new Date(),
+        sunSet       = new Date(),
+        firstRun     = true,
         turnOnLights = function() {
 
             var scheduleSettings = require('../scheduleSettings.json'),
@@ -118,12 +62,75 @@ var appSchedules = function(server) {
             });
         };
 
-    setSchedules();
-    // Set timer functions
-    dailyTimer = schedule.scheduleJob({hour: 2, minute: 0}, function(){
-        setSchedules();
-    });
+    // Setup daily timer function
+    rule.hour   = 20;
+    rule.minute = 30;
+    var dailyTimer = new schedule.scheduleJob(rule, function(){
 
+        //=========================================================
+        // Set the daily timers
+        //=========================================================
+        console.log (currentTime + ' - Running daily scheduler');
+
+        var currentTime      = dateFormat(new Date(), 'HH:MM'),
+            scheduleSettings = require('../scheduleSettings.json'),
+            turnOffTimes     = scheduleSettings.lightsOut,
+            turnOffTimers    = [],
+            turnOffTimer;
+
+        // Get sunrise & sunset data
+        const url = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=' + process.env.OPENWEATHERMAPAPIKEY;
+        alfredHelper.requestAPIdata(url)
+        .then(function(apiData){
+
+            sunRise = new Date(apiData.body.sys.sunrise);
+            sunSet  = new Date(apiData.body.sys.sunset);
+            sunSet.setHours(sunSet.getHours() + 12); // Add 12 hrs as for some resion the api returnes it as am!
+
+            // Cancel existing timers
+            if (!firstRun){
+                sunRiseTimer.cancel();
+                sunSetTimer.cancel();
+                turnOffTimers.forEach(function(value){
+                    value.cancel();
+                });
+            };
+            firstRun = false;                
+
+            // if sunrise is before 6am then reset to 6am
+            if (sunRise.getHours() < 6){
+                sunRise.setHours(6);
+                sunRise.setMinutes(0);
+            };
+
+            // Set sunrise timer
+            rule.hour   = sunRise.getHours();
+            rule.minute = sunRise.getMinutes();
+            var sunRiseTimer = new schedule.scheduleJob(rule, function(){
+                turnOnLights();
+            });
+
+            // Set sunset timer
+            rule.hour   = sunSet.getHours();
+            rule.minute = sunSet.getMinutes();
+            var sunSetTimer = new schedule.scheduleJob(rule, function(){
+                turnOnLights();
+            });
+
+            // set timers to turn off lights
+            turnOffTimes.forEach(function(value){
+                rule.hour    = value.hour;
+                rule.minute  = value.minute;
+                turnOffTimer = new schedule.scheduleJob(rule, function(){
+                    turnOffLights();
+                });
+                turnOffTimers.push(turnOffTimer);
+            });
+        })
+        .catch(function (err) {
+            console.log('Schedule get data Error: ' + err);
+        });
+    });
 };
 
 module.exports = appSchedules;
