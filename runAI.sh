afd730dd50bb7f04660c5eaa6a50db302c40e2ff
John Pillar <johneppillar@gmail.com>
2017-04-16 19:58:52 +0100

    Removed nodemon and made it start with node

--- runAI.sh
+++ runAI.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+clear
 echo "The following node processes were found:"
 ps aux | grep " node " | grep -v grep
 nodepids=$(ps aux | grep " node app.js " | grep -v grep | cut -c10-15)
@@ -7,16 +8,16 @@ echo "OK, so we will stop these process/es now..."
 
 kill -9 $(ps aux | grep '\snode\s' | awk '{print $2}')
 
-echo "done"
+echo "Done."
 
-echo "setting node to v7.5.0"
+echo "Now setting node to v7.5.0"
 source ~/.nvm/nvm.sh
 nvm use 7.5.0
 
-echo "removing node modules folder and installing latest"
-#rm -rf node_modules
-#npm install
+echo "Removing node modules folder and installing latest"
+rm -rf node_modules
+npm install
 
-echo "run the server app"
-nodemon server.js
-#node server.js
+echo "Run the server app"
+#nodemon server.js
+node server.js
