e1fe33a6b96b398c1695d1409bfcf16e49be6a16
John Pillar <johneppillar@gmail.com>
2017-07-23 07:30:40 +0100

    Added save morning to settings api

--- skills/lights/lightshelper.js
+++ skills/lights/lightshelper.js
@@ -285,7 +285,6 @@ exports.xy_to_rgb = function (x, y, brightness) {
 
 exports.rgb_to_xy = function (red, green, blue) {
 
-    /*
     //Apply a gamma correction to the RGB values, which makes the color more vivid and more the like the color displayed on the screen of your device
 	var red   = (red > 0.04045) ? Math.pow((red + 0.055) / (1.0 + 0.055), 2.4) : (red / 12.92);
 	var green = (green > 0.04045) ? Math.pow((green + 0.055) / (1.0 + 0.055), 2.4) : (green / 12.92);
@@ -303,26 +302,6 @@ exports.rgb_to_xy = function (red, green, blue) {
 	if (isNaN(x)) x = 0;
 
 	if (isNaN(y)) y = 0;	 
-*/
-
-    // better ver
-    if (red > 0.04045) { 
-        red = Math.pow((red + 0.055) / (1.0 + 0.055), 2.4);
-    } else red = (red / 12.92);
-
-    if (green > 0.04045) {
-        green = Math.pow((green + 0.055) / (1.0 + 0.055), 2.4);
-    } else green = (green / 12.92);
-
-    if (blue > 0.04045) {
-        blue = Math.pow((blue + 0.055) / (1.0 + 0.055), 2.4);
-    } else blue = (blue / 12.92);
-
-    var X = red * 0.664511 + green * 0.154324 + blue * 0.162028;
-    var Y = red * 0.283881 + green * 0.668433 + blue * 0.047685;
-    var Z = red * 0.000088 + green * 0.072310 + blue * 0.986039;
-    var x = X / (X + Y + Z);
-    var y = Y / (X + Y + Z);
 
     var rgbJSON = {
         x : x,
--- skills/settings/settings.js
+++ skills/settings/settings.js
@@ -6,7 +6,160 @@ const Skills       = require('restify-router').Router,
       skill        = new Skills();
 
 //=========================================================
-// Skill: settings
+// Skill: view settings
+//=========================================================
+function restoreSettings (req, res, next) {                        
+
+    var scheduleSettings = {
+    "morning": {
+        "on_hr": 5,
+        "on_min": 30,
+        "lights": [
+            {
+                "lightID": 1,
+                "onoff": "on",
+                "brightness": 135,
+                "x": 0.264,
+                "y": 0.3297,
+                "type": "color"
+            },
+            {
+                "lightID": 2,
+                "onoff": "off",
+                "brightness": 65,
+                "x": 0.264,
+                "y": 0.3297,
+                "type": "color"
+            },
+            {
+                "lightID": 3,
+                "onoff": "off",
+                "brightness": 65,
+                "x": 0.264,
+                "y": 0.3297,
+                "type": "color"
+            },
+            {
+                "lightID": 12,
+                "onoff": "on",
+                "brightness": 135,
+                "x": 0,
+                "y": 0,
+                "type": "white"
+            },
+            {
+                "lightID": 5,
+                "onoff": "on",
+                "brightness": "135",
+                "x": 0,
+                "y": 0,
+                "type": "white"
+            }
+        ],
+        "off_hr": 8,
+        "off_min": 30
+    },
+    "evening": {
+        "offset_min": 0,
+        "off_hr": 22,
+        "off_min": 0,
+        "lights": [
+            {
+                "x": 0.3633,
+                "lightID": 1,
+                "brightness": 120,
+                "onoff": "on",
+                "y": 0.3851,
+                "type": "color"
+            },
+            {
+                "x": 0.3633,
+                "lightID": 2,
+                "brightness": 120,
+                "onoff": "off",
+                "y": 0.3851,
+                "type": "color"
+            },
+            {
+                "x": 0.3633,
+                "lightID": 3,
+                "brightness": 120,
+                "onoff": "off",
+                "y": 0.3851,
+                "type": "color"
+            },
+            {
+                "x": 0,
+                "lightID": 12,
+                "brightness": 120,
+                "onoff": "on",
+                "y": 0,
+                "type": "white"
+            },
+            {
+                "x": 0,
+                "lightID": 5,
+                "brightness": 120,
+                "onoff": "off",
+                "y": 0,
+                "type": "white"
+            }
+        ],
+        "offset_hr": 0
+    },
+    "eveningtv": {
+        "on_hr": 19,
+        "on_min": 30,
+        "lights": [
+            {
+                "lightID": 2,
+                "onoff": "on",
+                "brightness": 50,
+                "x": 0.3633,
+                "y": 0.3851,
+                "type": "color"
+            },
+            {
+                "lightID": 3,
+                "onoff": "on",
+                "brightness": 50,
+                "x": 0.3633,
+                "y": 0.3851,
+                "type": "white"
+            },
+            {
+                "lightID": 12,
+                "onoff": "on",
+                "brightness": 50,
+                "x": 0,
+                "y": 0,
+                "type": "white"
+            }
+        ]
+    },
+    "motionSensorLights": [
+        {
+            "lightID": 1,
+            "brightness": 120,
+            "x": 0.3633,
+            "y": 0.3851,
+            "type": "color"
+        }
+    ]
+    }
+
+    // Restore settings file
+    try {
+        require('fs').writeFileSync('scheduleSettings.json', JSON.stringify(scheduleSettings));
+        alfredHelper.sendResponse(res, 'sucess', 'saved');
+    } catch (err) {
+        logger.error ('saveSettings: ' + err);
+        alfredHelper.sendResponse(res, 'error', err);
+    };
+}
+
+//=========================================================
+// Skill: view settings
 //=========================================================
 function viewSettings (req, res, next) {                        
 
@@ -21,6 +174,13 @@ function viewSettings (req, res, next) {
 
         // Add light name to json
         scheduleSettings.morning.lights[i]["lightName"] = alfredHelper.getLightName(value.lightID);
+
+        // Caculate and add rgb to json
+        rgb = lightshelper.xy_to_rgb(scheduleSettings.evening.lights[i]["x"], scheduleSettings.evening.lights[i]["y"], scheduleSettings.evening.lights[i]["brightness"])
+
+        scheduleSettings.evening.lights[i]["red"] = rgb.red;
+        scheduleSettings.evening.lights[i]["green"] = rgb.green;
+        scheduleSettings.evening.lights[i]["blue"] = rgb.blue;
 
         i++;
     })
@@ -49,6 +209,13 @@ function viewSettings (req, res, next) {
         // Add light name to json
         scheduleSettings.eveningtv.lights[i]["lightName"] = alfredHelper.getLightName(value.lightID);
 
+        // Caculate and add rgb to json
+        rgb = lightshelper.xy_to_rgb(scheduleSettings.evening.lights[i]["x"], scheduleSettings.evening.lights[i]["y"], scheduleSettings.evening.lights[i]["brightness"])
+
+        scheduleSettings.evening.lights[i]["red"] = rgb.red;
+        scheduleSettings.evening.lights[i]["green"] = rgb.green;
+        scheduleSettings.evening.lights[i]["blue"] = rgb.blue;
+
         i++;
     })
 
@@ -57,7 +224,48 @@ function viewSettings (req, res, next) {
 };
 
 //=========================================================
-// Skill: saveSettings
+// Skill: save morning settings
+//=========================================================
+function saveMorningSettings (req, res, next) {  
+    logger.info ('Save evening settings API called');
+
+    // Load settings json file
+    var scheduleSettings = JSON.parse(require('fs').readFileSync('scheduleSettings.json', 'utf8'));
+
+    // Update with data from api post
+    scheduleSettings.morning = req.body;
+
+    // Tidy up json by removing light name and RGB
+    var i = 0
+    scheduleSettings.morning.lights.forEach(function(value) {
+        
+        // Convert RGB to XY
+        xy = lightshelper.rgb_to_xy(scheduleSettings.morning.lights[i]["red"], scheduleSettings.morning.lights[i]["green"], scheduleSettings.morning.lights[i]["blue"])
+
+        scheduleSettings.morning.lights[i]["x"] = xy.x;
+        scheduleSettings.morning.lights[i]["y"] = xy.y;
+
+        // Remove light name & RGB data
+        delete scheduleSettings.morning.lights[i]["lightName"];
+        delete scheduleSettings.morning.lights[i]["red"];
+        delete scheduleSettings.morning.lights[i]["green"];
+        delete scheduleSettings.morning.lights[i]["blue"];
+
+        i++;
+    })
+
+    // Write updates to file
+    try {
+        require('fs').writeFileSync('scheduleSettings.json', JSON.stringify(scheduleSettings));
+        alfredHelper.sendResponse(res, 'sucess', 'saved');
+    } catch (err) {
+        logger.error ('saveMorningSettings: ' + err);
+        alfredHelper.sendResponse(res, 'error', err);
+    };
+};
+
+//=========================================================
+// Skill: save evening settings
 //=========================================================
 function saveEveningSettings (req, res, next) {  
     logger.info ('Save evening settings API called');
@@ -72,6 +280,12 @@ function saveEveningSettings (req, res, next) {
     var i = 0
     scheduleSettings.evening.lights.forEach(function(value) {
         
+        // Convert RGB to XY
+        xy = lightshelper.rgb_to_xy(scheduleSettings.evening.lights[i]["red"], scheduleSettings.evening.lights[i]["green"], scheduleSettings.evening.lights[i]["blue"])
+
+        scheduleSettings.evening.lights[i]["x"] = xy.x;
+        scheduleSettings.evening.lights[i]["y"] = xy.y;
+
         // Remove light name & RGB data
         delete scheduleSettings.evening.lights[i]["lightName"];
         delete scheduleSettings.evening.lights[i]["red"];
@@ -81,14 +295,12 @@ function saveEveningSettings (req, res, next) {
         i++;
     })
 
-logger.info (scheduleSettings.evening)
-
     // Write updates to file
     try {
         require('fs').writeFileSync('scheduleSettings.json', JSON.stringify(scheduleSettings));
         alfredHelper.sendResponse(res, 'sucess', 'saved');
     } catch (err) {
-        logger.error ('saveSettings: ' + err);
+        logger.error ('saveEveningSettings: ' + err);
         alfredHelper.sendResponse(res, 'error', err);
     };
 };
@@ -96,7 +308,9 @@ logger.info (scheduleSettings.evening)
 //=========================================================
 // Add skills to server
 //=========================================================
+skill.get('/restore', restoreSettings);
 skill.get('/view', viewSettings);
+skill.post('/savemorning', saveMorningSettings);
 skill.post('/saveevening', saveEveningSettings);
 
 module.exports = skill;
