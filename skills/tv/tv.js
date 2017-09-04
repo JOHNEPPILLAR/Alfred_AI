a244ad8b917ee7ec057767449d10051f3d01444b
JP <johneppillar@gmail.com>
2017-09-04 20:15:30 +0100

    Added watch Apple TV end point to TV skill

--- skills/tv/tv.js
+++ skills/tv/tv.js
@@ -6,7 +6,7 @@ const Skills  = require('restify-router').Router;
       harmony = require('harmonyhubjs-client');
 
 //=========================================================
-// Skill: watch fire tv
+// Skill: watch Amazon Fire TV
 //=========================================================
 function watchFireTv (req, res, next) {
 
@@ -27,7 +27,7 @@ function watchFireTv (req, res, next) {
 };
 
 //=========================================================
-// Skill: watch virgin tv
+// Skill: watch Virgin TV
 //=========================================================
 function watchVirginTv (req, res, next) {
 
@@ -77,7 +77,7 @@ function turnofftv (req, res, next) {
 
     harmony(process.env.harmonyip)
     .then(function(harmonyClient) {
-        harmonyClient.startActivity(-1); // Turn off TV ID
+        return harmonyClient.turnOff()
         harmonyClient.end()
         alfredHelper.sendResponse(res, 'sucess', 'Turned off TV');
     })
@@ -89,6 +89,25 @@ function turnofftv (req, res, next) {
     next();
 };
 
+function watchAppleTV (req, res, next) {
+    
+    logger.info ('Watch Apple TV API called');
+    
+        harmony(process.env.harmonyip)
+        .then(function(harmonyClient) {
+            harmonyClient.startActivity(22797639); // Apple TV ID
+            harmonyClient.end();
+            alfredHelper.sendResponse(res, 'sucess', 'Turned on Apple TV');
+        })
+        .catch(function (err) {
+            // Send response back to caller
+            alfredHelper.sendResponse(res, 'error', err.message);
+            logger.error('watchAppleTv: ' + err);
+        });
+        next();
+    
+}
+
 //=========================================================
 // Add skills to server
 //=========================================================
@@ -96,5 +115,6 @@ skill.get('/watchfiretv', watchFireTv);
 skill.get('/watchvirgintv', watchVirginTv);
 skill.get('/playps4', playps4);
 skill.get('/turnoff', turnofftv);
+skill.get('/watchappletv', watchAppleTV);
 
 module.exports = skill;
