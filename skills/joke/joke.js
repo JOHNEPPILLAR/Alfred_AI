8d88dfee85357f7499d349245a96f7bdd0e371fa
JP <johneppillar@gmail.com>
2017-09-24 10:39:27 +0100

    Converted to sync wait

--- skills/joke/joke.js
+++ skills/joke/joke.js
@@ -9,24 +9,23 @@ const skill = new Skills();
 /**
  * Skill: joke
  */
-function joke(req, res, next) {
+async function joke(req, res, next) {
   logger.info('Joke API called');
-
   const url = 'http://ron-swanson-quotes.herokuapp.com/v2/quotes';
-
-  alfredHelper.requestAPIdata(url)
-    .then((apiData) => {
-      // Get the joke data
-      apiData = apiData.body;
-      // Send response back to caller
-      alfredHelper.sendResponse(res, 'true', apiData);
-    })
-    .catch((err) => {
-      // Send response back to caller
-      alfredHelper.sendResponse(res, 'false', err.message);
-      logger.error(`joke: ${err}`);
-    });
-  next();
+  try {
+    const apiData = await alfredHelper.requestAPIdata(url);
+    apiData = apiData.body;
+    alfredHelper.sendResponse(res, 'true', apiData); // Send response back to caller
+    next();
+    return apiData;
+  } catch (err) {
+    if (typeof res !== 'undefined' && res !== null) {
+      alfredHelper.sendResponse(res, 'false', err); // Send response back to caller
+    }
+    logger.error(`joke: ${err}`);
+    next();
+    return err;
+  }
 }
 
 /**
