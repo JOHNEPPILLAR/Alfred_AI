f5aefe1aa5513b9a09bd858067f6c4dda976174a
JOHNEPPILLAR <johneppillar@gmail.com>
2020-01-18 11:54:57 +0000

    Removed .env

--- run.sh
+++ run.sh
@@ -15,6 +15,12 @@ snyk test
 echo "Set env vars"
 export ENVIRONMENT="development"
 export MOCK="false"
+export PORT=3978
+export ALFRED_FLOWERCARE_SERVICE="https://kidsroomserver:3981"
+export ALFRED_COMMUTE_SERVICE="https://alfred_commute_service:3979"
+export ALFRED_NETATMO_SERVICE="https://alfred_netatmo_data_collector_service:3979"
+export ALFRED_DYSON_SERVICE="https://alfred_dyson_data_collector_service:3979"
+export ALFRED_LIGHTS_SERVICE="https://alfred_lights_service:3979"
 
 echo "Run the server"
 npm run local
